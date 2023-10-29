e34a49c208074ea1c877d39b1a554bfc299f5fd9
Rocky Haotian Du <2712479005@qq.com>
2023-10-29 18:13:43 +0800

    Update Main.java

--- src/main/java/programming3/rocky/Main.java
+++ src/main/java/programming3/rocky/Main.java
@@ -1,7 +1,29 @@
 package programming3.rocky;
 
+import com.sun.net.httpserver.HttpServer;
+import programming3.rocky.httphandler.CommentHttpHandler;
+import programming3.rocky.httphandler.CoordinatesHttpHandler;
+import programming3.rocky.httphandler.RegistrationHttpHandler;
+import programming3.rocky.httphandler.WarningHttpHandler;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(1989), 0);
+
+        server.createContext("/comment", new CommentHttpHandler());
+        server.createContext("/coordinates", new CoordinatesHttpHandler());
+        server.createContext("/registration", new RegistrationHttpHandler());
+        server.createContext("/warning", new WarningHttpHandler());
+
+        server.setExecutor(null);  // creates a default executor
+
+        server.start();
+
+        System.out.println("Server started!");
     }
 }
