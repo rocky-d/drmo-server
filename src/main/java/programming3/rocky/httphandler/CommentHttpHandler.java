25a324d813ac2382cf60ad060b69e301bb6a6f06
Rocky Haotian Du <2712479005@qq.com>
2023-10-30 20:34:11 +0800

    Update classes in httphandler

--- src/main/java/programming3/rocky/httphandler/CommentHttpHandler.java
+++ src/main/java/programming3/rocky/httphandler/CommentHttpHandler.java
@@ -1,27 +1,4 @@
 package programming3.rocky.httphandler;
 
-import com.sun.net.httpserver.HttpExchange;
-
-import java.io.IOException;
-
 public class CommentHttpHandler extends HttpHandlerPrinciple {
-    @Override
-    public void handleGETRequest(HttpExchange httpExchange) {
-        // TODO
-    }
-
-    @Override
-    public void handlePOSTRequest(HttpExchange httpExchange) {
-        // TODO
-    }
-
-    @Override
-    public void handleNonSupportedRequest(HttpExchange httpExchange) {
-        super.handleNonSupportedRequest(httpExchange);
-    }
-
-    @Override
-    public void handle(HttpExchange httpExchange) throws IOException {
-        super.handle(httpExchange);
-    }
 }
