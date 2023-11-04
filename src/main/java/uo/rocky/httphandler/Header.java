c553126d359d3b6a2b4286a1f915e83fd8fe61ae
Rocky Haotian Du <2712479005@qq.com>
2023-11-04 18:24:36 +0800

    Update Header.java

--- src/main/java/uo/rocky/httphandler/Header.java
+++ src/main/java/uo/rocky/httphandler/Header.java
@@ -1,5 +1,31 @@
 package uo.rocky.httphandler;
 
 public enum Header {
-    ALLOW, CONTENT_ENCODING, CONTENT_LENGTH, CONTENT_TYPE, DATE, EXPIRES, LAST_MODIFIED, LOCATION, REFRESH, SERVER, SET_COOKIE, WWW_AUTHENTICATE;
+    ALLOW("Allow"),
+    CONTENT_ENCODING("Content-Encoding"),
+    CONTENT_LENGTH("Content-Length"),
+    CONTENT_TYPE("Content-Type"),
+    DATE("Date"),
+    EXPIRES("Expires"),
+    LAST_MODIFIED("Last-Modified"),
+    LOCATION("Location"),
+    REFRESH("Refresh"),
+    SERVER("Server"),
+    SET_COOKIE("Set-Cookie"),
+    WWW_AUTHENTICATE("WWW-Authenticate");
+
+    private final String STANDARD_NAME;
+
+    Header(final String STANDARD_NAME) {
+        this.STANDARD_NAME = STANDARD_NAME;
+    }
+
+    public String getSTANDARD_NAME() {
+        return STANDARD_NAME;
+    }
+
+    @Override
+    public String toString() {
+        return STANDARD_NAME;
+    }
 }
