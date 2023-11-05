83ae19aa1490179bc7c2d49b7479cd6715791ce3
Rocky Haotian Du <2712479005@qq.com>
2023-11-05 16:44:19 +0800

    Add UserAuthenticator.java

---
+++ src/main/java/uo/rocky/UserAuthenticator.java
@@ -0,0 +1,29 @@
+package uo.rocky;
+
+import com.sun.net.httpserver.BasicAuthenticator;
+import uo.rocky.entity.User;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UserAuthenticator extends BasicAuthenticator {
+    public UserAuthenticator(String realm) {
+        super(realm);
+    }
+
+    @Override
+    public boolean checkCredentials(String username, String password) {
+        Map<String, String> paramsMap = new HashMap<>();
+        paramsMap.put("QUERY", "username");
+        paramsMap.put("USERNAME", username);
+        try {
+            List<User> users = User.selectSQLite(paramsMap);
+            assert null != users;
+            return 1 == users.size() && password.hashCode() == users.get(0).getHashedpassword();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+}
