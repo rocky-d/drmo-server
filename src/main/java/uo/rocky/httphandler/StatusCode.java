15bd4af5de56777ebdb34a3711b7b5fed34490af
Rocky Haotian Du <2712479005@qq.com>
2023-11-06 21:33:27 +0800

    Update StatusCode.java

--- src/main/java/uo/rocky/httphandler/StatusCode.java
+++ src/main/java/uo/rocky/httphandler/StatusCode.java
@@ -4,6 +4,9 @@ import java.util.StringJoiner;
 
 public enum StatusCode {
     OK(200),
+    CREATED(201),
+    ACCEPTED(202),
+    NO_CONTENT(204),
     METHOD_NOT_ALLOWED(405),
     INTERNAL_SERVER_ERROR(500);
 
