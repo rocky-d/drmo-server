b0ae9fe68bc728be0dd13345ac929e5a512dbca1
Rocky Haotian Du <2712479005@qq.com>
2023-11-06 21:55:11 +0800

    Update StatusCode.java

--- src/main/java/uo/rocky/httphandler/StatusCode.java
+++ src/main/java/uo/rocky/httphandler/StatusCode.java
@@ -3,33 +3,39 @@ package uo.rocky.httphandler;
 import java.util.StringJoiner;
 
 public enum StatusCode {
-    OK(200),
-    CREATED(201),
-    ACCEPTED(202),
-    NO_CONTENT(204),
-    BAD_REQUEST(400),
-    UNAUTHORIZED(401),
-    FORBIDDEN(403),
-    NOT_FOUND(404),
-    METHOD_NOT_ALLOWED(405),
-    NOT_ACCEPTABLE(406),
-    REQUEST_TIMEOUT(408),
-    CONFLICT(409),
-    GONE(410),
-    LENGTH_REQUIRED(411),
-    INTERNAL_SERVER_ERROR(500),
-    NOT_IMPLEMENTED(501),
-    SERVICE_UNAVAILABLE(503),
-    HTTP_VERSION_NOT_SUPPORTED(505);
+    OK(200, ""),
+    CREATED(201, ""),
+    ACCEPTED(202, ""),
+    NO_CONTENT(204, ""),
+    BAD_REQUEST(400, ""),
+    UNAUTHORIZED(401, ""),
+    FORBIDDEN(403, ""),
+    NOT_FOUND(404, ""),
+    METHOD_NOT_ALLOWED(405, ""),
+    NOT_ACCEPTABLE(406, ""),
+    REQUEST_TIMEOUT(408, ""),
+    CONFLICT(409, ""),
+    GONE(410, ""),
+    LENGTH_REQUIRED(411, ""),
+    INTERNAL_SERVER_ERROR(500, ""),
+    NOT_IMPLEMENTED(501, ""),
+    SERVICE_UNAVAILABLE(503, ""),
+    HTTP_VERSION_NOT_SUPPORTED(505, "");
 
     private final int code;
+    private final String prompt;
 
-    StatusCode(int code) {
+    StatusCode(final int code, final String prompt) {
         this.code = code;
+        this.prompt = prompt;
     }
 
     public final int code() {
         return code;
+    }
+
+    public final String prompt() {
+        return prompt;
     }
 
     @Override
@@ -38,6 +44,7 @@ public enum StatusCode {
                 .add("name='" + name() + "'")
                 .add("ordinal=" + ordinal())
                 .add("code=" + code())
+                .add("prompt='" + prompt() + "'")
                 .toString();
     }
 }
