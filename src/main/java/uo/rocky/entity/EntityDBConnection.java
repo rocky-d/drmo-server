86da3e30547a8ef2dc258a75015d87a3854df6a5
Rocky Haotian Du <2712479005@qq.com>
2023-11-09 11:57:44 +0800

    Update EntityDBConnection.java

--- src/main/java/uo/rocky/entity/EntityDBConnection.java
+++ src/main/java/uo/rocky/entity/EntityDBConnection.java
@@ -17,29 +17,70 @@ public final class EntityDBConnection {
     }
 
     public static void setConnection(Connection connection) throws SQLException {
-        connection.setAutoCommit(false);
         EntityDBConnection.connection = connection;
-        setConnectionForAllEntities(connection);
+
+        setConnectionForAllEntities();
+
+        EntityDBConnection.connection.setAutoCommit(false);
+        createTablesIfNotExists();
     }
 
-    private static void setConnectionForAllEntities(Connection connection) {
+    private static void setConnectionForAllEntities() {
         Comment.setConnection(connection);
         Coordinate.setConnection(connection);
         User.setConnection(connection);
     }
 
+    private static void createTablesIfNotExists() throws SQLException {
+        Statement statement = connection.createStatement();
+        String createTableSQL;
+
+        createTableSQL = "CREATE TABLE IF NOT EXISTS user (\n" +
+                "    USR_NAME TEXT PRIMARY KEY NOT NULL UNIQUE,\n" +
+                "    USR_HASHEDPASSWORD INTEGER NOT NULL,\n" +
+                "    USR_EMAIL TEXT,\n" +
+                "    USR_PHONE TEXT\n" +
+                ");\n";
+        statement.addBatch(createTableSQL);
+
+        createTableSQL = "CREATE TABLE IF NOT EXISTS coordinate (\n" +
+                "    CDT_ID INTEGER PRIMARY KEY NOT NULL UNIQUE,\n" +
+                "    CDT_LONGITUDE REAL NOT NULL,\n" +
+                "    CDT_LATITUDE REAL NOT NULL,\n" +
+                "    CDT_DATETIME NUMERIC NOT NULL,\n" +
+                "    CDT_DANGERTYPE TEXT NOT NULL,\n" +
+                "    CDT_DESCRIPTION TEXT,\n" +
+                "    CDT_USR_NAME TEXT NOT NULL,\n" +
+                "    FOREIGN KEY (CDT_USR_NAME) REFERENCES user(USR_NAME)\n" +
+                ");\n";
+        statement.addBatch(createTableSQL);
+
+        createTableSQL = "CREATE TABLE IF NOT EXISTS comment (\n" +
+                "    CMT_ID INTEGER PRIMARY KEY NOT NULL UNIQUE,\n" +
+                "    CMT_CONTENT TEXT NOT NULL,\n" +
+                "    CMT_DATETIME NUMERIC NOT NULL,\n" +
+                "    CMT_CDT_ID INTEGER NOT NULL,\n" +
+                "    FOREIGN KEY (CMT_CDT_ID) REFERENCES coordinate(CDT_ID)\n" +
+                ");\n";
+        statement.addBatch(createTableSQL);
+
+        statement.executeBatch();
+        statement.close();
+        connection.commit();
+    }
+
     static synchronized List<Comment> selectComments(Map<String, String> params) throws SQLException {
-        String query;
+        String sql;
         Statement statement;
         ResultSet resultSet;
         List<Comment> results;
         switch (params.getOrDefault("QUERY", NO_QUERT_KEY).toUpperCase()) {
             case "COMMENTID":
-                query = "SELECT * FROM comment WHERE CMT_ID = " + params.get("COMMENTID") + ";";
-                System.out.println(query);
+                sql = "SELECT * FROM comment WHERE CMT_ID = " + params.get("COMMENTID") + ";";
+                System.out.println(sql);
 
                 statement = connection.createStatement();
-                resultSet = statement.executeQuery(query);
+                resultSet = statement.executeQuery(sql);
                 results = new ArrayList<>();
                 while (resultSet.next()) {
                     results.add(new Comment(
@@ -54,11 +95,11 @@ public final class EntityDBConnection {
                 connection.commit();
                 break;
             case "TIME":
-                query = ";";
-                System.out.println(query);
+                sql = ";";
+                System.out.println(sql);
 
                 statement = connection.createStatement();
-                resultSet = statement.executeQuery(query);
+                resultSet = statement.executeQuery(sql);
                 results = new ArrayList<>();
                 while (resultSet.next()) {
                     results.add(new Comment(
@@ -73,11 +114,11 @@ public final class EntityDBConnection {
                 connection.commit();
                 break;
             case "ID":
-                query = "SELECT * FROM comment WHERE CMT_CDT_ID = " + params.get("ID") + ";";
-                System.out.println(query);
+                sql = "SELECT * FROM comment WHERE CMT_CDT_ID = " + params.get("ID") + ";";
+                System.out.println(sql);
 
                 statement = connection.createStatement();
-                resultSet = statement.executeQuery(query);
+                resultSet = statement.executeQuery(sql);
                 results = new ArrayList<>();
                 while (resultSet.next()) {
                     results.add(new Comment(
@@ -92,11 +133,11 @@ public final class EntityDBConnection {
                 connection.commit();
                 break;
             case NO_QUERT_KEY:
-                query = "SELECT * FROM comment;";
-                System.out.println(query);
+                sql = "SELECT * FROM comment;";
+                System.out.println(sql);
 
                 statement = connection.createStatement();
-                resultSet = statement.executeQuery(query);
+                resultSet = statement.executeQuery(sql);
                 results = new ArrayList<>();
                 while (resultSet.next()) {
                     results.add(new Comment(
@@ -118,17 +159,17 @@ public final class EntityDBConnection {
     }
 
     static synchronized List<Coordinate> selectCoordinates(Map<String, String> params) throws SQLException {
-        String query;
+        String sql;
         Statement statement;
         ResultSet resultSet;
         List<Coordinate> results;
         switch (params.getOrDefault("QUERY", NO_QUERT_KEY).toUpperCase()) {
             case "ID":
-                query = "SELECT * FROM coordinate WHERE CDT_ID = " + params.get("ID") + ";";
-                System.out.println(query);
+                sql = "SELECT * FROM coordinate WHERE CDT_ID = " + params.get("ID") + ";";
+                System.out.println(sql);
 
                 statement = connection.createStatement();
-                resultSet = statement.executeQuery(query);
+                resultSet = statement.executeQuery(sql);
                 results = new ArrayList<>();
                 while (resultSet.next()) {
                     results.add(new Coordinate(
@@ -147,12 +188,12 @@ public final class EntityDBConnection {
                 connection.commit();
                 break;
             case "LOCATION":
-                query = "SELECT * FROM coordinate WHERE " + params.get("DOWNLONGITUDE") + " <= CDT_LONGITUDE AND CDT_LONGITUDE <= " + params.get("UPLONGITUDE") +
+                sql = "SELECT * FROM coordinate WHERE " + params.get("DOWNLONGITUDE") + " <= CDT_LONGITUDE AND CDT_LONGITUDE <= " + params.get("UPLONGITUDE") +
                         " AND " + params.get("DOWNLATITUDE") + " <= CDT_LATITUDE AND CDT_LATITUDE <= " + params.get("UPLATITUDE") + ";";
-                System.out.println(query);
+                System.out.println(sql);
 
                 statement = connection.createStatement();
-                resultSet = statement.executeQuery(query);
+                resultSet = statement.executeQuery(sql);
                 results = new ArrayList<>();
                 while (resultSet.next()) {
                     results.add(new Coordinate(
@@ -171,11 +212,11 @@ public final class EntityDBConnection {
                 connection.commit();
                 break;
             case "TIME":
-                query = ";";
-                System.out.println(query);
+                sql = ";";
+                System.out.println(sql);
 
                 statement = connection.createStatement();
-                resultSet = statement.executeQuery(query);
+                resultSet = statement.executeQuery(sql);
                 results = new ArrayList<>();
                 while (resultSet.next()) {
                     results.add(new Coordinate(
@@ -194,12 +235,12 @@ public final class EntityDBConnection {
                 connection.commit();
                 break;
             case "USER":
-                query = "SELECT * FROM coordinate WHERE CDT_USR_NAME = " +
+                sql = "SELECT * FROM coordinate WHERE CDT_USR_NAME = " +
                         EntityRelatesToSQL.escapeSingleQuotes(params.get("USERNAME")) + ";";
-                System.out.println(query);
+                System.out.println(sql);
 
                 statement = connection.createStatement();
-                resultSet = statement.executeQuery(query);
+                resultSet = statement.executeQuery(sql);
                 results = new ArrayList<>();
                 while (resultSet.next()) {
                     results.add(new Coordinate(
@@ -218,11 +259,11 @@ public final class EntityDBConnection {
                 connection.commit();
                 break;
             case NO_QUERT_KEY:
-                query = "SELECT * FROM coordinate;";
-                System.out.println(query);
+                sql = "SELECT * FROM coordinate;";
+                System.out.println(sql);
 
                 statement = connection.createStatement();
-                resultSet = statement.executeQuery(query);
+                resultSet = statement.executeQuery(sql);
                 results = new ArrayList<>();
                 while (resultSet.next()) {
                     results.add(new Coordinate(
@@ -248,18 +289,18 @@ public final class EntityDBConnection {
     }
 
     static synchronized List<User> selectUsers(Map<String, String> params) throws SQLException {
-        String query;
+        String sql;
         Statement statement;
         ResultSet resultSet;
         List<User> results;
         switch (params.getOrDefault("QUERY", NO_QUERT_KEY).toUpperCase()) {
             case "USERNAME":
-                query = "SELECT * FROM user WHERE USR_NAME = " +
+                sql = "SELECT * FROM user WHERE USR_NAME = " +
                         EntityRelatesToSQL.escapeSingleQuotes(params.get("USERNAME")) + ";";
-                System.out.println(query);
+                System.out.println(sql);
 
                 statement = connection.createStatement();
-                resultSet = statement.executeQuery(query);
+                resultSet = statement.executeQuery(sql);
                 results = new ArrayList<>();
                 while (resultSet.next()) {
                     results.add(new User(
@@ -274,11 +315,11 @@ public final class EntityDBConnection {
                 connection.commit();
                 break;
             case "HASHEDPASSWORD":
-                query = "SELECT * FROM user WHERE USR_HASHEDPASSWORD = " + params.get("HASHEDPASSWORD") + ";";
-                System.out.println(query);
+                sql = "SELECT * FROM user WHERE USR_HASHEDPASSWORD = " + params.get("HASHEDPASSWORD") + ";";
+                System.out.println(sql);
 
                 statement = connection.createStatement();
-                resultSet = statement.executeQuery(query);
+                resultSet = statement.executeQuery(sql);
                 results = new ArrayList<>();
                 while (resultSet.next()) {
                     results.add(new User(
@@ -293,12 +334,12 @@ public final class EntityDBConnection {
                 connection.commit();
                 break;
             case "EMAIL":
-                query = "SELECT * FROM user WHERE USR_EMAIL = " +
+                sql = "SELECT * FROM user WHERE USR_EMAIL = " +
                         EntityRelatesToSQL.escapeSingleQuotes(params.get("EMAIL")) + ";";
-                System.out.println(query);
+                System.out.println(sql);
 
                 statement = connection.createStatement();
-                resultSet = statement.executeQuery(query);
+                resultSet = statement.executeQuery(sql);
                 results = new ArrayList<>();
                 while (resultSet.next()) {
                     results.add(new User(
@@ -313,12 +354,12 @@ public final class EntityDBConnection {
                 connection.commit();
                 break;
             case "PHONE":
-                query = "SELECT * FROM user WHERE USR_PHONE = " +
+                sql = "SELECT * FROM user WHERE USR_PHONE = " +
                         EntityRelatesToSQL.escapeSingleQuotes(params.get("PHONE")) + ";";
-                System.out.println(query);
+                System.out.println(sql);
 
                 statement = connection.createStatement();
-                resultSet = statement.executeQuery(query);
+                resultSet = statement.executeQuery(sql);
                 results = new ArrayList<>();
                 while (resultSet.next()) {
                     results.add(new User(
@@ -333,11 +374,11 @@ public final class EntityDBConnection {
                 connection.commit();
                 break;
             case NO_QUERT_KEY:
-                query = "SELECT * FROM user;";
-                System.out.println(query);
+                sql = "SELECT * FROM user;";
+                System.out.println(sql);
 
                 statement = connection.createStatement();
-                resultSet = statement.executeQuery(query);
+                resultSet = statement.executeQuery(sql);
                 results = new ArrayList<>();
                 while (resultSet.next()) {
                     results.add(new User(
