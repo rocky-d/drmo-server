7ec73cc45b37bf34259e51ce9fc4269e1ae179d3
Rocky Haotian Du <2712479005@qq.com>
2023-11-13 15:59:02 +0800

    Update UserAuthenticator.java

--- src/main/java/uo/rocky/UserAuthenticator.java
+++ src/main/java/uo/rocky/UserAuthenticator.java
@@ -4,9 +4,9 @@ import com.sun.net.httpserver.BasicAuthenticator;
 import uo.rocky.entity.User;
 
 import java.sql.SQLException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class UserAuthenticator extends BasicAuthenticator {
     public UserAuthenticator(String realm) {
@@ -15,11 +15,8 @@ public final class UserAuthenticator extends BasicAuthenticator {
 
     @Override
     public boolean checkCredentials(String username, String password) {
-        Map<String, String> queryParameters = new HashMap<>();
-        queryParameters.put("QUERY", "username");
-        queryParameters.put("USERNAME", username);
         try {
-            List<User> users = User.selectUserList(queryParameters);
+            List<User> users = User.selectUserList(Stream.of(new String[]{"QUERY", "USERNAME"}, new String[]{"USERNAME", username}).collect(Collectors.toMap(pair -> pair[0], pair -> pair[1])));
             return null != users && 1 == users.size() && password.hashCode() == users.get(0).getHashedpassword();
         } catch (SQLException sqlException) {
             // TODO
