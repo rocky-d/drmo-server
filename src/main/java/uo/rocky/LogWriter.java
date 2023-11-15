5698365355031534f630884faa9bf0c28270615a
Rocky Haotian Du <2712479005@qq.com>
2023-11-15 18:08:52 +0800

    Update LogWriter.java

--- src/main/java/uo/rocky/LogWriter.java
+++ src/main/java/uo/rocky/LogWriter.java
@@ -3,27 +3,30 @@ package uo.rocky;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.OpenOption;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.nio.file.StandardOpenOption.APPEND;
+import static java.nio.file.StandardOpenOption.CREATE;
 
 public final class LogWriter {
+    private static final OpenOption[] OPEN_OPTIONS = new StandardOpenOption[]{APPEND, CREATE};
+
     private static BufferedWriter bufferedWriter = null;
 
-    public static BufferedWriter getBufferedWriter() {
+    public static synchronized BufferedWriter getBufferedWriter() {
         return bufferedWriter;
     }
 
-    public static void setBufferedWriter(Path logFile) throws IOException {
-        bufferedWriter = Files.newBufferedWriter(logFile, UTF_8, APPEND);
+    public static synchronized void setBufferedWriter(Path logFile) throws IOException {
+        bufferedWriter = Files.newBufferedWriter(logFile, UTF_8, OPEN_OPTIONS);
     }
 
-    public static synchronized void write(String content) throws IOException {
-        bufferedWriter.write(content);
-    }
-
-    public static synchronized void flush() throws IOException {
+    public static synchronized void appendEntry(String entry) throws IOException {
+        bufferedWriter.write(entry);
+        bufferedWriter.newLine();
         bufferedWriter.flush();
     }
 
