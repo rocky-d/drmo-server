ed869714c02c3fb8c06e0c02f53381d398130027
Rocky Haotian Du <2712479005@qq.com>
2023-11-15 11:22:44 +0800

    Update ServerLauncher.java

--- src/main/java/uo/rocky/ServerLauncher.java
+++ src/main/java/uo/rocky/ServerLauncher.java
@@ -85,11 +85,11 @@ public final class ServerLauncher {
 
         try {
             loadConfig();
-            System.out.println("Load config done.");
+            System.out.println("Config was loaded.");
             System.out.println("---------");
 
             EntityDBConnection.setConnection(DriverManager.getConnection(sqliteUrl));
-            System.out.println(sqliteUrl + " connected.");
+            System.out.println(sqliteUrl + " was connected.");
             System.out.println("---------");
 
             if (!isHttps) {
