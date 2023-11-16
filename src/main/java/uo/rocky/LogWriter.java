3a583ac0830ef33f6900fc2e2a4b7f32358d0d6d
Rocky Haotian Du <2712479005@qq.com>
2023-11-16 17:02:44 +0800

    Update LogWriter.java (javadoc)

--- src/main/java/uo/rocky/LogWriter.java
+++ src/main/java/uo/rocky/LogWriter.java
@@ -84,7 +84,7 @@ public final class LogWriter {
     }
 
     /**
-     * Enumerates the types of log entries.
+     * Represents the types of log entries.
      */
     public enum LogEntryType {
 
