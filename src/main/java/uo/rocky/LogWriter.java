bc5ef21e56ac3f5c8643b627a2b138edace10fe1
Rocky Haotian Du <2712479005@qq.com>
2023-11-16 11:35:54 +0800

    Update LogWriter.java (add doc comments)

--- src/main/java/uo/rocky/LogWriter.java
+++ src/main/java/uo/rocky/LogWriter.java
@@ -16,19 +16,41 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.nio.file.StandardOpenOption.APPEND;
 import static java.nio.file.StandardOpenOption.CREATE;
 
+/**
+ * Handles the writing of log entries to a log file.
+ *
+ * <p>All fields and methods are static.
+ *
+ * @author Rocky Haotian Du
+ */
 public final class LogWriter {
     private static final OpenOption[] OPEN_OPTIONS = new StandardOpenOption[]{APPEND, CREATE};
 
     private static BufferedWriter bufferedWriter = null;
 
+    /**
+     * @return The BufferedWriter instance for writing log entries.
+     */
     public static synchronized BufferedWriter getBufferedWriter() {
         return bufferedWriter;
     }
 
+    /**
+     * Sets up the BufferedWriter for writing logs to the specified file.
+     *
+     * @param logFile The path to the log file.
+     * @throws IOException If an I/O error occurs while setting up the writer.
+     */
     public static synchronized void setBufferedWriter(Path logFile) throws IOException {
         bufferedWriter = Files.newBufferedWriter(logFile, UTF_8, OPEN_OPTIONS);
     }
 
+    /**
+     * Appends a log entry of a specified type with provided content.
+     *
+     * @param logEntryType The type of log entry (INFO, WARNING, ERROR).
+     * @param content      The content to be logged, represented as one or more strings.
+     */
     public static synchronized void appendEntry(LogEntryType logEntryType, String... content) {
         String temp;
         try {
@@ -51,10 +73,18 @@ public final class LogWriter {
         }
     }
 
+    /**
+     * Closes the BufferedWriter.
+     *
+     * @throws IOException If an I/O error occurs while closing the writer.
+     */
     public static synchronized void close() throws IOException {
         bufferedWriter.close();
     }
 
+    /**
+     * Enumerates the types of log entries.
+     */
     public enum LogEntryType {
         INFO,
         WARNING,
@@ -63,6 +93,11 @@ public final class LogWriter {
         LogEntryType() {
         }
 
+        /**
+         * Overrides toString() to provide details about the LogEntryType.
+         *
+         * @return A string representation of the type.
+         */
         @Override
         public final String toString() {
             return new StringJoiner(", ", Coordinate.Dangertype.class.getSimpleName() + "{", "}")
