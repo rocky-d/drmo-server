1fa19e5e49166606e72fec9f0955591205c0b5f0
Rocky Haotian Du <2712479005@qq.com>
2023-11-19 16:21:59 +0800

    Update LogWriter.java

--- src/main/java/uo/rocky/LogWriter.java
+++ src/main/java/uo/rocky/LogWriter.java
@@ -54,8 +54,10 @@ public final class LogWriter {
      * @param messages     the messages to be logged, represented as one or more strings.
      */
     public static synchronized void appendEntry(LogEntryType logEntryType, String... messages) {
+        final String RESET_CODE = "\u001B[0m";
         String temp;
         try {
+            System.out.print(logEntryType.STYLE_CODE);
             System.out.println(temp = "<" + logEntryType.name() + "> " + ZonedDateTime.now());
             bufferedWriter.write(temp);
             bufferedWriter.newLine();
@@ -65,13 +67,16 @@ public final class LogWriter {
                 bufferedWriter.newLine();
             }
             bufferedWriter.flush();
+            System.out.print(RESET_CODE);
         } catch (IOException ioException) {
+            System.out.print("\u001B[35m");
             System.out.println("*********");
             System.out.println(Arrays.toString(messages));
             System.out.println("*********");
             System.out.println("Write log failed.");
             System.out.println(ioException.getClass().getName() + ": " + ioException.getMessage());
-            ioException.printStackTrace();
+            System.out.print(RESET_CODE);
+            ioException.printStackTrace(System.err);
             System.exit(-1);
         }
     }
@@ -93,20 +98,23 @@ public final class LogWriter {
         /**
          * The info type indicating the normal actions.
          */
-        INFO(),
+        INFO("\u001B[32m"),
         /**
          * The warning type indicating the improper actions causing not serious consequences.
          */
-        WARNING(),
+        WARNING("\u001B[33m"),
         /**
          * The error type indicating the exceptions that may affect the server's operations.
          */
-        ERROR();
+        ERROR("\u001B[31m");
+
+        public final String STYLE_CODE;
 
         /**
          * Constructs the types above.
          */
-        LogEntryType() {
+        LogEntryType(final String STYLE_CODE) {
+            this.STYLE_CODE = STYLE_CODE;
         }
 
         /**
@@ -119,6 +127,7 @@ public final class LogWriter {
             return new StringJoiner(", ", LogEntryType.class.getSimpleName() + "{", "}")
                     .add("name='" + name() + "'")
                     .add("ordinal=" + ordinal())
+                    .add("STYLE_CODE='" + STYLE_CODE + "'")
                     .toString();
         }
     }
