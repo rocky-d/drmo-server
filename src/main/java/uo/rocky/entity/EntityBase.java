ef15623ebe2203cfba3b067c038c2cbeeddbb1d4
Rocky Haotian Du <2712479005@qq.com>
2023-11-20 10:29:16 +0800

    Update EntityBase.java

--- src/main/java/uo/rocky/entity/EntityBase.java
+++ src/main/java/uo/rocky/entity/EntityBase.java
@@ -1,17 +1,15 @@
 package uo.rocky.entity;
 
 import java.sql.Connection;
-import java.time.format.DateTimeFormatter;
 
 /**
  * TODO
+ * <p>
+ * Implements {@link EntityRelatesToJSON}, {@link EntityRelatesToSQL}.
  *
  * @author Rocky Haotian Du
  */
 public abstract class EntityBase implements EntityRelatesToJSON, EntityRelatesToSQL {
-
-//    public static final DateTimeFormatter LOCALDATETIME_FORMATTER_T = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS");
-//    public static final DateTimeFormatter LOCALDATETIME_FORMATTER_SPACE = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
 
     protected static Connection getConnection() {
         return EntityDBConnection.getConnection();
